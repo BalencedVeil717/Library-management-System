9ec59ad6b0d2b055cc8d02bb5f6eb77684dd01f9
BalencedVeil717 <abhinavkumar150807@gmail.com>
2024-11-29 20:27:49 +0530

    added function to delete old loan

---
+++ loan/return.py
@@ -0,0 +1,27 @@
+# REMOVING LOAN DETAILS
+def delete_loan():
+    from config import connectSQL
+
+    # Connection Establishment
+    myCon = connectSQL()
+    myCur = myCon.cursor()
+
+    # Creating Interface
+    print("] RETURNING BOOKS |\n")
+    loanID = int(input("\tEnter Loan ID : "))
+
+    # Formulating Query
+    query = "DELETE FROM loans WHERE loan_id = {}".format(loanID)
+
+    # Applying Changes
+    try:
+        myCur.execute(query)
+        myCon.commit()
+        print("Loan Removed Successfully!")
+    except:
+        print("An Error Occurred!")
+    finally:
+        myCon.close()
+
+
+delete_loan()
