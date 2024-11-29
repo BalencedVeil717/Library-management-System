8523b84a4224a81223153b1cf9cb3a79218ab252
BalencedVeil717 <abhinavkumar150807@gmail.com>
2024-11-29 19:52:46 +0530

    added delete function for members table

---
+++ member/delete.py
@@ -0,0 +1,25 @@
+# DELETING MEMBER RECORDS
+def delete():
+    from config import connectSQL
+
+    # Connection Establishment
+    myCon = connectSQL()
+    myCur = myCon.cursor()
+
+    # Creating Interface
+    print("] DELETING MEMBER RECORD |\n")
+    memberID = int(input("\tEnter Member ID: "))
+
+    # Formulating Query
+    query = "DELETE FROM members WHERE member_id = {}".format(memberID)
+
+    # Applying Changes
+    try:
+        myCur.execute(query)
+        myCon.commit()
+        print("Deleted Record Successfully!")
+    except:
+        print("An Error Occurred!")
+    finally:
+        myCon.close()
+
